606aecb48a8d2da457fdcb50b4744baf7054a2c7
estaudere <66801479+estaudere@users.noreply.github.com>
2021-01-25 00:57:41 -0600

    add revenue chart

--- javascript/d3-practice/budget/budget.js
+++ javascript/d3-practice/budget/budget.js
@@ -80,6 +80,92 @@ function drawBudgetChartGeneral() {
     });
 }
 
+
+function drawRevenueChart() {
+    // constant margins, width, and height
+    var margin = {top: 50, right: 200, bottom: 20, left: 0};
+    var width = 1000 - margin.left - margin.right;
+    var height = 120 - margin.top - margin.bottom;
+
+    // set up our scales
+    var x = d3.scaleLinear().range([0, width]);
+
+    // make our svgs
+    var svgRevenue = d3.select(".budget-charts").append("div").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom);
+
+
+    // add a title for the main bar
+    svgRevenue.append("text")
+        .attr("class", "chart-title")
+        .attr("x", 0)
+        .attr("y",  margin.top / 2)
+        .text("Revenue Sources");
+
+
+
+
+    // add in our general revenue data and plot!!
+    d3.csv("general_revenue.csv").then(function(data) {
+
+        // format the data
+        data.forEach(function(d) {
+            d.projected = +d["Amount Projected"];
+            d.name = d["Source"];
+        });
+
+        // add the stack attributes to the data
+        const total = d3.sum(data, d => d.projected);
+        let value = 0;
+        var stack = data.map(d => ({
+            name: d.name,
+            amount: d.projected,
+            value: d.projected / total,
+            startValue: (value / total),
+            endValue: (value += d.projected) / total
+        }));
+
+        var bars = svgRevenue.append("g")
+            .selectAll("rect")
+            .data(stack)
+            .enter()
+            .append("rect")
+            .attr("fill", function(data) {
+                if (data.name == "Deficit") return "var(--secondary-light)";
+                else return "var(--secondary)";
+            })
+            .attr("x", d => x(d.startValue))
+            .attr("y", margin.top)
+            .attr("width", d => x(d.endValue) - x(d.startValue) - 1)
+            .attr("height", height);
+        var tooltip = generateTooltip(svgRevenue);
+
+        bars.on("mouseover", function() {
+            d3.select(this).transition()
+                .attr("fill", "#7a6673");
+            tooltip.attr("display", "inline");
+        })
+        .on("mouseout", function() {
+            d3.select(this).transition()
+                .attr("fill", function(data) {
+                    if (data.name == "Deficit") return "var(--secondary-light)";
+                    else return "var(--secondary)";
+                });
+            tooltip.attr("display", "none");
+        })
+        .on("mousemove", function(event, data) {
+            var x0 = d3.pointer(event, this)[0],
+            y0 = d3.pointer(event, this)[1];
+
+            tooltip.attr("transform", `translate(${x0}, ${y0})`);
+            tooltip.select(".tooltip-group").text(data.name);
+            tooltip.select(".tooltip-amount").text(formatCurrency(data.amount));
+            });
+        
+    });
+}
+
 function drawBudgetChartsSecondary() {
 
     var margin = {top: 50, right: 200, bottom: 20, left: 0};
